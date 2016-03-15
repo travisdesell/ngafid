6806931689b86f719f26b5db92b1f2b52e900207
fdm <fdm@fdm.und.edu>
2016-03-15 05:06:57 +0000

    add airport model

---
+++ app/Airports.php
@@ -0,0 +1,10 @@
+<?php namespace NGAFID;
+
+use Illuminate\Auth\Authenticatable;
+use Illuminate\Database\Eloquent\Model;
+
+
+class Airports extends Model {
+
+    protected $table = 'airports';
+}
