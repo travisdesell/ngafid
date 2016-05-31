998d774f3ab3808b784991e81a68dd12a980b4ed
fdm <fdm@fdm.und.edu>
2016-05-31 12:29:50 +0000

    update maintenance message

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -8,7 +8,14 @@
                     <div class="panel-heading"><b>Service Unavailable</b> <span class="pull-right">{{date("D M d, Y G:i A T")}}</span></div>
 
                     <div class="panel-body">
-                        <p>The NGAFID is currently down for maintenance. Please try again later.</p>
+                        <p>The National General Aviation Flight Information Database will be performing maintenance on
+                            May 31 to June 1, 2016. During that time, access to our website will be temporarily unavailable.
+                            We appreciate your patience as we perform these upgrades to better serve you.</p>
+
+                        <p>Users who participate via the GAARD app may continue to record their flights using their mobile device.
+                            The services offered by GAARD will not be affected by this scheduled maintenance.</p>
+
+                        <p>We apologize for any inconvenience caused, and thank you for your patience.</p>
                     </div>
                 </div>
             </div>
