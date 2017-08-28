55bf400733fe14f45fd3ce950011e7b260bdeeee
Unknown <kelton.karboviak@gmail.com>
2017-08-28 02:36:09 -0500

    Modified the css/js links in master blade to use elixir gen'd scripts
    + Removed the bootstrap-toggle css/js links
    + Added in links for the locally generated css/js files by elixir.

--- resources/views/NGAFID-master.blade.php
+++ resources/views/NGAFID-master.blade.php
@@ -4,11 +4,15 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+
     <title>NGAFID</title>
+
     <link rel="shortcut icon" href="{{ asset('images/favicon.ico') }}">
-    <link href="{{ asset('/css/app.css') }}" rel="stylesheet">
-    <link href="{{ asset('/css/custom.css') }}" rel="stylesheet">
-    <link href="https://gitcdn.github.io/bootstrap-toggle/2.2.2/css/bootstrap-toggle.min.css" rel="stylesheet">
+
+    <!-- Styles -->
+    {{--<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">--}}
+    <link href="{{ elixir("css/vendor.css") }}" rel="stylesheet">
+    <link href="{{ elixir("css/custom.css") }}" rel="stylesheet">
 
     @yield('cssScripts')
 
@@ -109,9 +113,9 @@
     @yield('content')
 
     <!-- Scripts -->
-    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-    <script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.1/js/bootstrap.min.js"></script>
-    <script src="https://gitcdn.github.io/bootstrap-toggle/2.2.2/js/bootstrap-toggle.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    <script src="{{ elixir("js/vendor.js") }}"></script>
 
     <script>
         $(function () {
